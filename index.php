5f5b90b8962f849db0ac45365435a7da1d465ad6
hossein-highbye <hosseinbarzegari1414@gmail.com>
2023-10-01 14:23:49 +0330

    localized bootstrap 5.2.3

--- index.php
+++ index.php
@@ -39,8 +39,7 @@ if (isset($_POST['finalSubmit']) || $_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 ?>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<link rel="stylesheet" href="assets/css/bootstrap-5.2.3.css">
 <link rel="stylesheet" href="assets/css/styles.css">
 <script type="text/javascript" src="assets/Inputmask/dist/jquery.inputmask.js"></script>
 <script type="text/javascript" src="assets/persianDatepicker/js/persianDatepicker.min.js"></script>
